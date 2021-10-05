4b2be7e67a112856fbc6438381693d244a964962
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-10-05 13:00:56 +0200

    cleaned up code

--- Session1/Rock-paper-scissors/src/main/kotlin/Main.kt
+++ Session1/Rock-paper-scissors/src/main/kotlin/Main.kt
@@ -2,45 +2,20 @@ fun main () {
     val a = arrayOf("Rock", "Paper", "Scissors")
     var randomChoice = a.random()
     var result = "undecided"
-    var score = 0
     val win = "You win!"
     val lose = "You lose!"
     val draw = "It's a draw!"
     val choice = readLine()
 
-    if (choice == "rock") {
-        if (randomChoice == "Paper") {
-            result = lose
-            score -= 1
-        }
-        if (randomChoice == "Scissors") {
-            result = win
-            score += 1
-        }
-    }
-    if (choice == "paper") {
-        if (randomChoice == "Rock") {
-            result = win
-            score += 1
-        }
-        if (randomChoice == "Scissors") {
-            result = lose
-            score -= 1
-        }
-    }
-    if (choice == "scissors") {
-        if (randomChoice == "Rock") {
-            result = lose
-            score -= 1
-        }
-        if (randomChoice == "Paper") {
-            result = win
-            score += 1
-        }
-    }
     if (randomChoice == choice?.capitalize()) {
-        result = draw
+        return println("It's a draw!")
     }
+    when(randomChoice){
+        "Rock" -> if(choice == "paper"){result = win} else{result = lose}
+        "Paper" -> if(choice == "scissors"){result = win} else{result = lose}
+        "Scissors" -> if(choice == "rock"){result = win} else{result = lose}
+    }
+
     if (choice == "paper" || choice == "rock" || choice == "scissors") {
         println("You: ${choice?.capitalize()}")
         println("Opponent: ${randomChoice}")
