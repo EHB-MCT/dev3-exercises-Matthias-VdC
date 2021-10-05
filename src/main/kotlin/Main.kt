d709ec5779bcf818ffb14a408af593647d46e0de
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-10-05 11:24:05 +0200

    hello world done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
-fun main(){
-    println("Hello world")
+fun main() {
+    var names = arrayOf("Sam", "Matthias", "Rayan", "Dries", "Guillaume")
+    for(name in names) {
+        println("Hello $name!")
+    }
 }
