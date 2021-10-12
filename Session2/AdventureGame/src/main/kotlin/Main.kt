6e3260d2d1139eb0cad4484b497be17f2df4fc6c
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-10-12 12:31:08 +0200

    Challenge 3 done
    Completed challenge 3.
    If you fail an answer you do not get the next challenge.

--- Session2/AdventureGame/src/main/kotlin/Main.kt
+++ Session2/AdventureGame/src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 fun main(){
     println("You are stuck in a tower, the only way to escape is by going up...")
     challengeOne()
-    challengeTwo()
 }
 
 fun gameOver(){
-    return println("You have failed. Game Over!")
+    println("You have failed. Game Over!")
 }
 
 fun succes(){
@@ -24,11 +23,13 @@ fun diceRoll():Int{
 fun challengeOne(){
     floorCount("1")
     println("Solve this riddle to proceed: Feed me and I will live, give me a drink and I will die. What am I?")
-
     var answer = readLine()
 
     when(answer){
-        "fire" -> succes()
+        "fire" -> {
+            succes()
+            challengeTwo()
+        }
         else -> {
             gameOver()
         }
@@ -39,21 +40,56 @@ fun challengeOne(){
 fun challengeTwo (){
     floorCount("2")
     println("Guess what number the dice will fall on, and you may pass.")
-
     val answer = readLine()
     val roll = diceRoll()
-
     println("The dice has fallen on $roll")
+
     when(answer) {
         "1", "2", "3" -> if (roll == 1 || roll == 2 || roll == 3) {
             succes()
+            challengeThree()
         } else {
             gameOver()
         }
-        else -> if (roll == 4 || roll == 5 || roll == 6) {
+        "4", "5", "6" -> if (roll == 4 || roll == 5 || roll == 6) {
             succes()
+            challengeThree()
         } else {
             gameOver()
+        }
+    }
+}
+
+fun challengeThree(){
+    floorCount("3")
+    println("You arrive before a giant rat who seems hostile. You see 3 items sprawled in front of you on the floor.")
+    println("Which item do you pick?")
+    println("1. The whip")
+    println("2. The bow")
+    println("3. An axe")
+    println("Type in the number of what weapon you choose.")
+
+    val answer = readLine()
+
+    when(answer){
+        "1" -> {
+            println("The rat screams at you but cowers back in fear. It seems to have been whipped before.")
+            succes()
+            challengeFour()
+        }
+        "2" -> {
+            println("You take the bow and the rat charges at you.")
+            println("You don't have any arrows and end up getting eaten by the rat")
+            gameOver()
+            challengeFour()
+        }
+        "3" -> {
+            println("You take the axe and charge at the rat.")
+            println("You stab it with the axe and wound it.")
+            println("The rat screams but is even further enraged.")
+            println("He bites you and you black out.")
+            gameOver()
+            challengeFour()
         }
     }
 }
