f44ecc61175ba6406ee5171f7845ac2cf253d2cc
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-10-19 10:23:42 +0200

    Clean up - client brief

--- Session3/ClientBrief/src/main/kotlin/Main.kt
+++ Session3/ClientBrief/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
-val design = Modules("Design 1", 5, "Je maakt in dit opleidingsonderdeel kennis met de basisprincipes van grafische vormgeving: typografie, kleur, beeld, compositie en de werkprocessen van een grafisch vormgever. We leren deze principes toepassen en maken daarvoor gebruik van de software uit de professionele wereld (Adobe Illustrator en Photoshop).")
-val development = Modules("Development 1", 5, "Je leert procedureel programmeren met behulp van Javascript. Door gebruik te maken van de grafische mogelijkheden van HTML 5 Canvas leer je op een visuele en intuïtieve manier programmeren. Alle basisconcepten van programmeren komen aan bod: variabelen en objecten, functies en methodes, variatie door middel van iteratiestructuren en selectiestructuren, events en lijst-structuren. Met behulp van de aangeleerde vaardigheden genereer je abstracte afbeeldingen en interactieve animaties.")
-
+fun main() {
+    val design1 = Modules(
+        "Design 1",
+        1,
+        "Maaike",
+        5,
+        "Je maakt in dit opleidingsonderdeel kennis met de basisprincipes van grafische vormgeving: typografie, kleur, beeld, compositie en de werkprocessen van een grafisch vormgever. We leren deze principes toepassen en maken daarvoor gebruik van de software uit de professionele wereld (Adobe Illustrator en Photoshop).",
+        1,
+        "creative",
+        true,
+    )
+    val development1 = Modules(
+        "Development 1",
+        1,
+        "unknown",
+        5,
+        "Je leert procedureel programmeren met behulp van Javascript. Door gebruik te maken van de grafische mogelijkheden van HTML 5 Canvas leer je op een visuele en intuïtieve manier programmeren. Alle basisconcepten van programmeren komen aan bod: variabelen en objecten, functies en methodes, variatie door middel van iteratiestructuren en selectiestructuren, events en lijst-structuren. Met behulp van de aangeleerde vaardigheden genereer je abstracte afbeeldingen en interactieve animaties.",
+        1,
+        "coding",
+        true
+    )
+}
--- Session3/ClientBrief/src/main/kotlin/Modules.kt
+++ Session3/ClientBrief/src/main/kotlin/Modules.kt
@@ -1,3 +1,12 @@
-class Modules (val course: String?, val credits: Int?, val description: String?){
+class Modules (
+    val courseName: String?,
+    val semester: Int?,
+    val teacher: String?,
+    val credits: Int?,
+    val description: String?,
+    val year: Int?,
+    val module: String?,
+    val required: Boolean?
+    ){
 
 }
