2ba460c460c4a0ad347bae0aa8e7ba440b0b31bb
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-11-23 12:53:21 +0100

    Finished exercise 1 & 2 :)

--- public_transport/src/main/kotlin/Main.kt
+++ public_transport/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import java.sql.*
 import java.util.*
 
+
 fun installer(){
     Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
     val connectionProps = Properties()
@@ -10,10 +11,27 @@ fun installer(){
 // Create the connection: this will allow us to run queries on it later
     val connection =  DriverManager.getConnection(
         "jdbc:" + "mysql" + "://" +
-                "dt5.ehb.be" +
+                Credentials().hostName +
                 ":" + "3306" + "/" +
                 Credentials().user,
         connectionProps)
+
+    //1st exercise
+    var statement = connection.prepareStatement("SELECT DISTINCT city_b FROM trains")
+    var result = statement.executeQuery()
+    println("please choose the following destinations:")
+    while(result.next()) {
+        println(result.getString("city_b"))
+    }
+
+    //2nd exercise
+    val chooseDestination = readLine()
+
+    statement = connection.prepareStatement("SELECT * FROM trains WHERE city_b='$chooseDestination' AND CURRENT_DATE = DATE(departure) ORDER BY departure ASC limit 3")
+    result = statement.executeQuery()
+    while(result.next()) {
+        println(result.getString("train_name") + " - Platform: " + result.getString("platform") +  " - Departure time: " + result.getString("departure"))
+    }
 }
 
 fun main(){
